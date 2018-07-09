7d24219a98d5e582fb15e65ec4127f5fa6e734c3
Debmalya <debmalyasarkar1@gmail.com>
2018-07-09 14:27:34 +0530

    Initial Version

---
+++ elinux-reqpkg.sh
@@ -0,0 +1,5 @@
+sudo apt-get update
+
+sudo apt-get install -y libncurses-dev vim bison flex texinfo zlibc zlib1g zlib1g-dev libglib2.0-dev gawk automake libtool cvs ctags cscope mtd-utils u-boot-tools minicom xinetd tftpd tftp nfs-kernel-server git g++ libssl-dev
+
+sudo apt-get install -y gcc-multilib texinfo unzip diffstat git-core wget gawk build-essential chrpath socat libsdl1.2-dev xterm lzop
